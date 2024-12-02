a0a6ce576fe1b170b11fdeb1ccc656451d6c4221
Максим <kirshin.99@mail.ru>
2024-12-02 20:14:10 +0500

    Lynophrina

---
+++ scripts/facts.js
@@ -0,0 +1,14 @@
+let randomFacts = [
+    "When I was a child I used to love commercials on TV and would start crying if someone turned them off.",
+    "I make checklists all the time. That’s why my desk is covered with notes.",
+    "I always buy food that I don't like to make sure I still don't like it",
+    "I feel ashamed when my tweets are read out loud to me. But I still love writing them.",
+    "It seems to me that someday I will give up all my achievements and get a bachelor's degree in architecture",
+    "When the plane takes off, I turn on the song “Familiar” by Agnes Obel at 1:20", 
+    "My favorite book is 'Extremely Loud & Incredibly Close'. I have read it over ten times.",
+    "I believe we're living in a simulation",
+    "I plan to get three dogs and call them Bubble, Doodle and Paradidl"
+  ]; 
+  let randomFact = randomFacts[Math.floor(Math.random() * randomFacts.length)];
+  
+//   console.log(randomFact);
